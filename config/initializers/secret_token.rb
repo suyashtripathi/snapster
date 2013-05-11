33fe5f55d127044f42db249d782636d4cd631e03
Suyash Tripathi <suyashtripathi@gmail.com>
2013-05-11 22:49:20 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snapster::Application.config.secret_token = 'caf31d0210bb1fde861c7e7ea50dc83dda95a2b7918642c376d6bb973f24375b3209c0ffe0924614de46da042755f9be3b8008f508f69236d987bf48f6885702'
